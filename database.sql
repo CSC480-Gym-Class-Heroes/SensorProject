64bfea887326a771701d352ed5f8b7fe7e8657f0
Chaskin Saroff <csaroff@oswego.edu>
2015-04-03 17:36:16 -0400

    Added a Markdown file describing how to update the gym class schedule information.

--- database.sql
+++ database.sql
@@ -1,46 +1,12 @@
-create database if not exists csc480;
+CREATE database IF NOT EXISTS csc480;
 
-use csc480;
+USE csc480;
 
-
-
-drop table if exists gym;
-
-create table gym(
-    gymID int,
-    gymName varchar(20),
-    primary key (gymID));
-
- 
-drop table if exists sensorData;
+DROP TABLE if EXISTS sensorData;
    
-create table sensorData(
-    dataID int,
-    weekDay varchar(10),
-    sensorTimeStamp DATETIME,
-    sensorInOut bit,
-    gymID int,
-    primary key (dataID),
-    FOREIGN KEY (gymID) REFERENCES gym(gymID));
-    
-drop table if exists class;
-
-create table class(
-    classID int,
-    className varchar(40),
-    classDesc varchar(512),
-    primary key classID);
-    
-drop table if exists gymClass;
-
-create table gymClass(
-    gymID int,
-    classID int,
-    weekDay int,
-    startTime TIME,
-    endTime TIME,
-    primary key (gymID,classID),
-    FOREIGN KEY (gymID) REFERENCES gym(gymID),
-    FOREIGN KEY (classID) REFERENCES class(classID));
-    
-    
+CREATE TABLE sensorData (
+    sensorTimeStamp BIGINT,
+    count int,
+    gymID varchar(15),
+    primary key (sensorTimeStamp, gymID)
+);
